93a17e30253d9748f94e4f22593ef49e12ec041f
Dirkmal <kamsam403@gmail.com>
2025-04-13 16:19:15 +0100

    Report Form Complete. Stylings Next

--- src/app/features/reports/report-form/report-form.component.ts
+++ src/app/features/reports/report-form/report-form.component.ts
@@ -29,6 +29,8 @@ export class ReportFormComponent implements OnInit {
   detailsForm!: FormGroup;
   macroscopyForm!: FormGroup;
   microscopyForm!: FormGroup;
+  ihcForm!: FormGroup;
+  pathologistForm!: FormGroup;
   currentStep = 0;
 
   // sub-forms for macroscopy
@@ -37,7 +39,13 @@ export class ReportFormComponent implements OnInit {
   axillary_proSF!: FormGroup;
 
   // sub-forms for microscopy
-
+  in_situSF!: FormGroup;
+  invasive_carcinomaSF!: FormGroup;
+  axillary_nodeSF!: FormGroup;
+  marginSF!: FormGroup;
+  surgical_marginSF!: FormGroup;
+  pathological_stagingSF!: FormGroup;
+  
 
   specimen_type_options: string[] = Object.values(SpecimenTypes);
   axillary_procedure_options: string[] = Object.values(AxillaryProcedures);
@@ -60,6 +68,7 @@ export class ReportFormComponent implements OnInit {
     new DynamicControl({ controlType: ControlTypes.SELECT, name: 'side', label: 'Laterality', options: this.laterality_options}),      
   ];
   
+  /** MACROSCOPY */
   specimen_type_controls = [
     new DynamicControl({ name: 'core_needle_biopsy', controlType: ControlTypes.CHECKBOX }),
     new DynamicControl({ name: 'wide_local_excision', controlType: ControlTypes.CHECKBOX }),
@@ -84,8 +93,9 @@ export class ReportFormComponent implements OnInit {
 		new DynamicControl({ name: 'intrammary_node', controlType: ControlTypes.CHECKBOX }),
   ];
 
+  /** MICROSCOPY */
   in_situ_carcinoma_controls = [
-    new DynamicControl({ name: 'dcis', label: 'Dual Carcinoma In-situ (DCIS) size', type: InputTypes.NUMBER }),
+    new DynamicControl({ name: 'dcis', label: 'Dual Carcinoma In-situ (DCIS) size', type: InputTypes.NUMBER, required: true }),
     new DynamicControl({ name: 'lobular', label: 'Lobular Carcinoma In-situ', controlType: ControlTypes.CHECKBOX }),
     new DynamicControl({ name: 'paget', label: 'Paget Disease', controlType: ControlTypes.CHECKBOX }),
     new DynamicControl({ name: 'microinvasion', label: 'Microinvasion present', controlType: ControlTypes.CHECKBOX }),
@@ -109,6 +119,48 @@ export class ReportFormComponent implements OnInit {
     new DynamicControl({ name: 'number_positive', type: InputTypes.NUMBER })
   ];
 
+  margin_controls = [
+    new DynamicControl({ name: 'excision_margins' }),
+    new DynamicControl({ name: 'skin_involvement', controlType: ControlTypes.SELECT, options: this.skin_involvement_options }),
+    new DynamicControl({ name: 'nipple_involvement', controlType: ControlTypes.CHECKBOX }),
+    new DynamicControl({ name: 'skeletal_muscle_involvement', controlType: ControlTypes.SELECT, options: this.skeletal_options }),
+    new DynamicControl({ name: 'surgical_margins', controlType: ControlTypes.SELECT, options: this.skin_involvement_options }),
+  ];
+
+  surgical_margin_controls = [
+    new DynamicControl({ name: 'superior', controlType: ControlTypes.CHECKBOX }),
+		new DynamicControl({ name: 'inferior', controlType: ControlTypes.CHECKBOX }),
+		new DynamicControl({ name: 'anterior', controlType: ControlTypes.CHECKBOX }),
+		new DynamicControl({ name: 'posterior', controlType: ControlTypes.CHECKBOX }),
+		new DynamicControl({ name: 'lateral', controlType: ControlTypes.CHECKBOX }),
+    new DynamicControl({ name: 'medial', controlType: ControlTypes.CHECKBOX })
+  ]
+
+  pathological_staging_controls = [
+    new DynamicControl({ name: 'pt_not_applicable', label: 'not applicable', controlType: ControlTypes.CHECKBOX }),
+    new DynamicControl({ name: 'pt', type: InputTypes.NUMBER }),
+    new DynamicControl({ name: 'n', type: InputTypes.NUMBER }),
+    new DynamicControl({ name: 'm', type: InputTypes.NUMBER }),
+  ]
+
+  /** IHC **/
+	ihc_controls = [
+		new DynamicControl({ name: 'oestrogen_receptor_status', controlType: ControlTypes.SELECT, options: this.ihc_options }),
+		new DynamicControl({ name: 'pr', controlType: ControlTypes.SELECT, options: this.ihc_options }),
+		new DynamicControl({ name: 'her2', controlType: ControlTypes.SELECT, options: this.ihc_options }),
+		new DynamicControl({ name: 'quick_allred_score', type: InputTypes.NUMBER })
+	];
+
+	/** Pathologist report **/
+	pathologist_report_controls = [
+		new DynamicControl({ name: 'final_diagnosis', required: true, controlType: ControlTypes.TEXTAREA}),
+		new DynamicControl({ name: 'comment', required: true, controlType: ControlTypes.TEXTAREA}),
+		new DynamicControl({ name: 'consultant_pathologist', required: true}),
+		new DynamicControl({ name: 'date_of_request', controlType: ControlTypes.DATE, required: true}),
+		new DynamicControl({ name: 'date_received', controlType: ControlTypes.DATE, required: true}),
+		new DynamicControl({ name: 'date_reviewed', controlType: ControlTypes.DATE, required: true}),	
+	];
+
   constructor(
     private fb: FormBuilder,
     private snackBar: MatSnackBar,
@@ -122,40 +174,39 @@ export class ReportFormComponent implements OnInit {
   initForms(): void {
     this.detailsForm = this.dcs.toFormGroup(this.detail_controls);
     
+    /** MACROSCOPY */
     this.specimen_typeSF = this.dcs.toFormGroup(this.specimen_type_controls);
     this.specimen_dimSF = this.dcs.toFormGroup(this.specimen_dimension_controls);
     this.axillary_proSF = this.dcs.toFormGroup(this.axillary_procedure_controls);
 
-    this.macroscopyForm = new FormGroup({'specimen_type': this.specimen_typeSF});
-		this.macroscopyForm.addControl('specimen_dimensions', this.specimen_dimSF);
-		this.macroscopyForm.addControl('axillary_procedure', this.axillary_proSF) ;
+    this.macroscopyForm =this.fb.group({
+      specimen_type: this.specimen_typeSF,
+		  specimen_dimensions: this.specimen_dimSF,
+		  axillary_procedure: this.axillary_proSF
+    }) ;
 
-
+    /** MICROSCOPY */
+    this.in_situSF = this.dcs.toFormGroup(this.in_situ_carcinoma_controls);
+    this.invasive_carcinomaSF = this.dcs.toFormGroup(this.invasive_carcinoma_controls);
+    this.axillary_nodeSF = this.dcs.toFormGroup(this.axillary_node_controls);
+    this.marginSF = this.dcs.toFormGroup(this.margin_controls);
+    this.surgical_marginSF = this.dcs.toFormGroup(this.surgical_margin_controls);
+    this.pathological_stagingSF = this.dcs.toFormGroup(this.pathological_staging_controls);
 
     this.microscopyForm = this.fb.group({
-      dcis: ['', [Validators.required, Validators.min(0)]],
-      in_situ_carcinoma: this.fb.array(
-        this.in_situ_carcinoma_options.map(() => this.fb.control(false))
-      ),
-      microinvasion: [''],
-      ic_present: [''],
-      invasive_tumor_size: ['', [Validators.min(0)]],
-      whole_tumor_size: ['', [Validators.min(0)]],
-      invasive_carcinoma_type: this.fb.array(
-        this.invasive_carcinoma_options.map(() => this.fb.control(false))
-      ),
-      invasive_grade: this.fb.array(
-        this.invasive_grades_options.map(() => this.fb.control(false))
-      ),
-      sbr_score: ['', [Validators.min(0)]],
-      tumour_extent: this.fb.array(
-        this.tumor_extent_options.map(() => this.fb.control(false))
-      ),
-      lympho_vascular_invasion: this.fb.array(
-        this.lympho_options.map(() => this.fb.control(false))
-      ),
-      site_of_other_nodes: ['']
+      in_situ: this.in_situSF,
+      invasive_carcinoma: this.invasive_carcinomaSF,
+      axillary_node: this.axillary_nodeSF,
+      margin: this.marginSF,
+      surgical_margin: this.surgical_marginSF,
+      pathological_staging: this.pathological_stagingSF
     });
+
+    /** IHC */
+    this.ihcForm = this.dcs.toFormGroup(this.ihc_controls);
+
+    /** PATHOLOGIST */
+    this.pathologistForm = this.dcs.toFormGroup(this.pathologist_report_controls);
   }
 
   get specimenTypesFormArray(): FormArray {
